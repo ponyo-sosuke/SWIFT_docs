b0e4ec27bf3efa0e6a588dcdcc123c7f8a4fc971
ponyo-sosuke (doe) <95349554+ponyo-sosuke@users.noreply.github.com>
2021-12-12 17:06:09 +0100

    exemple

---
+++ Exemple_ShowingSheet.swift
@@ -0,0 +1,71 @@
+Exemple Swift
+
+//
+//  mainView.swift
+//  test
+//
+//  Created by dorothee on 25/10/2021.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    @State var showingSheet: Bool = false
+    var body: some View {
+
+        Button(action:{
+            self.showingSheet.toggle() })
+               {
+            Text(" appeller la sheetView")
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(12)}
+
+        .sheet(isPresented: $showingSheet){
+            modalView()
+        }
+    }
+
+struct mainView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainView()
+    }
+}
+}
+//
+//  modalView.swift
+//  test
+//
+//  Created by dorothee on 25/10/2021.
+//
+
+import SwiftUI
+
+struct modalView: View {
+
+    @Environment(.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack{
+            Text("Je suis une sheet view")
+
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()},
+                   label: {
+                Text("Quitter")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+
+            })
+        }
+    }
+}
+
+struct modalView_Previews: PreviewProvider {
+    static var previews: some View {
+        modalView()
+    }
+}
